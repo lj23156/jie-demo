9579a9928f119ed22f18f26b6c97dc3584798b67
jie <1411068461@qq.com>
2021-08-21 18:21:24 +0800

    init

---
+++ src/main/java/com/jie/JieDemoApplication.java
@@ -0,0 +1,13 @@
+package com.jie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JieDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JieDemoApplication.class, args);
+    }
+
+}
